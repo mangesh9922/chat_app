d07ff8c6ba6f3644959e3a4e929053a5f1e3437c
Muxx <gajakoshmangesh2020@gmail.com>
2024-03-21 18:28:48 +0530

    Added textfield and defind css properties - Added textfield for password - Added css properties for card and box

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,16 +2,52 @@ import React from 'react'
 import TextField from '@mui/material/TextField'
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
+import Box from '@mui/material/Box'
 
 function Login() {
   return (
-      <div className='login'>
-         
-          <Card sx={{ minWidth: 275 }} >
-              <CardContent>
-                  <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-              </CardContent>
-         </Card>
+    <div
+      className='login'
+      display="flex"
+      height={"100vh"}
+      width={"100vw"}
+      bgcolor="lightgreen"
+      alignItems="center"
+      justifyContent="center">
+      <Box
+        height={"97vh"}
+        width={"98vw"}
+
+        // my={4}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      // gap={4}
+      // p={2}
+      // sx={{ border: '2px solid grey' }}
+      >
+        <Card
+
+          sx={{ minWidth: 275, maxWidth: 345 }} >
+          <CardContent
+            display="flex"
+            alignItems="center"
+            justifyContent = "center"
+            sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}
+          >
+            <TextField
+              id="Username"
+              label="Username"
+              variant="outlined"
+            />
+            <TextField
+              id="Password"
+              label="Password"
+              variant="outlined"
+            />
+          </CardContent>
+        </Card>
+      </Box>
     </div>
   )
 }
