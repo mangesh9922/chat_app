293c0bf41f89d0ffe995d8994cac8d3d799ebae2
dipak6962 <dipakhirve99@gmail.com>
2024-03-22 17:15:11 +0530

    Added Routes (success) - install packages and code review - routes is running successfully

--- src/index.js
+++ src/index.js
@@ -6,19 +6,21 @@ import Login from './components/Login';
 import {
 
   createBrowserRouter,
+  createRoutesFromElements,
+  Route,
   RouterProvider,
 } from "react-router-dom";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello </div> ,
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={ <App />}/>
+
+  )
+);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router}/>  
-    {/* <App /> */}
+    <App />
   </React.StrictMode>
 );
 
